86387dab69576cd776b10b9028355b920365717e
Boowie41 <160162527+Boowie41@users.noreply.github.com>
2024-03-02 03:34:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,20 @@ for (const file of eventFiles) {
   }
 }
 
+const { exec } = require("child_process");
+
+exec("node deploy-commands.js", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
 client.login(token);
 
 
